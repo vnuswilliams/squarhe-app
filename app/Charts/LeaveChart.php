52f3a6c4c19580c43d97d39cc7b9b089e8f53295
vnuswilliams <106296680+vnuswilliams@users.noreply.github.com>
2026-05-23 04:22:07 +0100

    Update LeaveChart.php

--- app/Charts/LeaveChart.php
+++ app/Charts/LeaveChart.php
@@ -14,18 +14,28 @@ public Company $company
     public function leavePerDepertment(): \vnusWilliams\LarapexCharts\BarChart
     {
         $stats = $this->company->employees()
-                ->join('leaves', 'employees.id', '=', 'leaves.employee_id')
-                ->selectRaw('employees.department, SUM(leaves.days) as total_days')
-                ->groupBy('employees.department')
-                ->reorder()
-                ->get();
+    ->join('leaves', 'employees.id', '=', 'leaves.employee_id')
+    ->selectRaw('employees.department, SUM(leaves.days) as total_days')
+    ->groupBy('employees.department')
+    ->reorder()
+    ->get();
 
-        return (new LarapexChart)->barChart()
-            ->setTitle('Absences ou congé par département')
-            ->addData($stats->pluck('total_days')->toArray(), 'Total jour absence')
-            ->setXAxis( array_unique($stats->pluck('department')->toArray()), "Département")
-            ->setToolbar(show: true)
-            ->setGrid();
+return (new LarapexChart)->barChart()
+    ->setTitle('Absences ou congé par département')
+    ->addData(
+        $stats->pluck('total_days')->toArray(),
+        'Total jour absence'
+    )
+    ->setXAxis(
+        $stats->pluck('department')
+            ->map(fn ($department) => $department->value)
+            ->unique()
+            ->values()
+            ->toArray(),
+        'Département'
+    )
+    ->setToolbar(show: true)
+    ->setGrid();
 
     }
 }
